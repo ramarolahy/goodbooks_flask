658606b76b325830e6d43977b1c6ebc1a2ed0440
Mbinintsoa Ramarolahy <mbinintsoa.ramarolahy@gmail.com>
2019-02-28 14:33:04 -0700

    hid sensitive data

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask, session
 from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
@@ -14,3 +17,13 @@ app.config.from_object('config')
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
--- app/models.py
+++ app/models.py
@@ -1,16 +1,21 @@
-import os
+from app import db
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 
-DATABASE_URL = "postgres://btetampwgolbgl: 4fdc0ef18ff204c01816a6677422c6ef4f44771fae9518aeae8ae9b40e8839f3" \
-               " @ ec2 - 107 - 20 - 185 - 27.compute - 1.amazonaws.com: 5432 / d7jkbrbmpshg7n"
+class Book(db.Model):
+    __tablename__ = "books"
+    isbn = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    review_count = db.Column(db.Integer, nullable=False)
+    ratings_count = db.Column(db.Integer, nullable=False)
+    ratings_average = db.Column(db.Integer, nullable=False)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
 
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False )
+
 
