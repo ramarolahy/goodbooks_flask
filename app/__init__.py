6e6796442d741fa14498d3f7cb4a6976072e99e8
Mbinintsoa Ramarolahy <mbinintsoa.ramarolahy@gmail.com>
2019-02-28 17:08:29 -0700

    setting up database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
-import os
+
 # Third party imports
-from flask import Flask, session
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 # from flask_session import Session
@@ -9,8 +11,16 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from config import app_config
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
+
+# Check for environment variable
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
+
+db = SQLAlchemy()
+
+login_Manager = LoginManager()
 
 # Initialize the app
 def create_app(config_name):
@@ -21,15 +31,17 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
 
+    login_Manager.init_app(app)
+    login_Manager.login_message = "Please login to view this page"
+    login_Manager.login_view = "auth.login"
+
     # Configure session to use filesystem
     app.config["SESSION_PERMANENT"] = False
     app.config["SESSION_TYPE"] = "filesystem"
     #db.init_app(app)
     #Session(app)
 
-    @app.route('/')
-    def books():
-        return 'Books are nice!!'
+
 
     return app
 
@@ -37,11 +49,4 @@ def create_app(config_name):
 
 
 
-
-
-
-
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
 
--- app/models.py
+++ app/models.py
@@ -1,19 +1,62 @@
-from app import db
+# Third party imports
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+# Local imports
+from app import db, login_Manager
 
 
 class Book(db.Model):
+    """
+    CREATE A BOOKS TABLE
+    """
     __tablename__ = "books"
-    isbn = db.Column(db.Integer, nullable=False)
+
+    isbn = db.Column(db.Integer, unique=True, nullable=False)
     title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer, nullable=False)
+    reviews = db.relationship("Users", backref="books", lazy=True)
 
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
+    """
+    CREATE AN EMPLOYEE TABLE
+    """
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False )
 
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(55), index=True, unique=True, nullable=False)
+    username = db.Column(db.String(30), index=True, unique=True, nullable=False)
+    first_name = db.Column(db.String(30), index=True, nullable=False)
+    last_name = db.Column(db.String(30), index=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    books_reviewed = db.Column(db.Integer, db.ForeignKey('books.isbn'), unique=True)
+
+    @property
+    def password(self):
+        """
+        PREVENT PASSWORD FROM BEING ACCESSED
+        :return: Error Message
+        """
+        raise AttributeError('Password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        """
+        HASH USER PASSWORD
+        :param password: User password
+        :return: hashed password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        CHECK IF HASHED PASSWORD MATCHES USER PASSWORD
+        :param password:
+        :return: boolean
+        """
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.username)
 
